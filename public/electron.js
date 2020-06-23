eb131890b81d62aecf88aed26aafab29880d3cbe
dorne <dorne.wang@gmail.com>
2020-06-23 17:30:03 +0800

    run test

--- public/electron.js
+++ public/electron.js
@@ -24,7 +24,7 @@ function createWindow () {
   );
     
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  if(isDev) mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
