9f38faaf394dc31167aeb73f206448d1f2fba30f
dorne <dorne.wang@gmail.com>
2020-06-23 22:26:59 +0800

    app component

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          We are using Node.js
-          <span id="node-version"></span>, Chromium <span id="chrome-version"></span>, and Electron{' '}
-          <span id="electron-version"></span>.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            We are using Node.js
+            <span id="node-version"></span>, Chromium <span id="chrome-version"></span>, and Electron{' '}
+            <span id="electron-version"></span>.
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
