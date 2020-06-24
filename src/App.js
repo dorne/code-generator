fe5b83f44868b16eaacb0af3a175a1c46203222f
dorne <dorne.wang@gmail.com>
2020-06-24 18:39:33 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Tooltip } from 'antd';
+import 'antd/dist/antd.css';
 
 class App extends React.Component {
   handleClick(e) {
     //alert('click');
     if (window.electron) {
       const { dialog } = window.electron.remote;
-      window.electron.remote.getCurrentWindow().setSize(2000, 1000)
+      window.electron.remote.getCurrentWindow().setSize(2000, 1000);
       dialog.showErrorBox('提示', '支持electron');
-    }else{
+    } else {
       alert('不支持electron');
     }
   }
@@ -31,11 +33,19 @@ class App extends React.Component {
             Learn React
           </a>
           <p>
-            <button onClick={this.handleClick}>btn</button>
+           
             We are using Node.js
             <span id="node-version"></span>, Chromium <span id="chrome-version"></span>, and
             Electron <span id="electron-version"></span>.
           </p>
+
+          <Tooltip title="search">
+            <Button type="primary" onClick={this.handleClick}>electron event</Button>
+          </Tooltip>
+
+          <Tooltip title="search">
+            <Button type="primary">Button</Button>
+          </Tooltip>
         </header>
       </div>
     );
