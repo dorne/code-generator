4b3df9df3c09e426e48e436e96d26e9095b96ad7
dorne <dorne.wang@gmail.com>
2020-07-07 14:31:11 +0800

    表单

--- src/pages/project/Edit.jsx
+++ src/pages/project/Edit.jsx
@@ -2,20 +2,121 @@ import React from 'react';
 
 import { baseComponent } from '../../components/hof/base';
 
+import { Form, Input, Button, Select } from 'antd';
+
+const { Option } = Select;
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 18,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 class Edit extends React.Component {
+  formRef = React.createRef();
+
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  componentDidMount() {
-  }
+  onGenderChange = value => {
+    this.formRef.current.setFieldsValue({
+      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+    });
+  };
+
+  onFinish = values => {
+    console.log(values);
+  };
+
+  onReset = () => {
+    this.formRef.current.resetFields();
+  };
+
+  onFill = () => {
+    this.formRef.current.setFieldsValue({
+      note: 'Hello world!',
+      gender: 'male',
+    });
+  };
+
+  componentDidMount() {}
 
   render() {
-   
     return (
       <div>
-       project edit
+        <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
+          <Form.Item
+            name="note"
+            label="Note"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="gender"
+            label="Gender"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Select
+              placeholder="Select a option and change input text above"
+              onChange={this.onGenderChange}
+              allowClear
+            >
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+              <Option value="other">other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            noStyle
+            shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+          >
+            {({ getFieldValue }) =>
+              getFieldValue('gender') === 'other' ? (
+                <Form.Item
+                  name="customizeGender"
+                  label="Customize Gender"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              ) : null
+            }
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button htmlType="button" onClick={this.onReset}>
+              Reset
+            </Button>
+            <Button type="link" htmlType="button" onClick={this.onFill}>
+              Fill form
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
